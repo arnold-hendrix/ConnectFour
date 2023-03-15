b5efa9bbb10341a5d04ee7bf6fbd619475e57bb4
Arnold Masanga <arnomas101@gmail.com>
2023-03-15 13:54:55 -0400

    create Player class

--- src/com/arnold/connect/Player.java
+++ src/com/arnold/connect/Player.java
@@ -1,5 +1,35 @@
 package com.arnold.connect;
 
+import java.util.Scanner;
+
 public class Player {
+	private String name;
+	private String playerNumber;
+
+	private static Scanner scanner = new Scanner(System.in);
+
+	public Player(String name, String playerNumber) {
+		this.name = name;
+		this.playerNumber = playerNumber;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getPlayerNumber() {
+		return playerNumber;
+	}
+
+	public int makeMove() {
+		System.out.println("Select a column for your token: ");
+		int column = scanner.nextInt();
+		return column;
+	}
+
+	@Override
+	public String toString() {
+		return "Player " + playerNumber + " is " + name;
+	}
 
 }
